189342bb50617a2964bb5b5978453a6cc0dd446e
Sebastian Klocke <info@sebastiankuhnow.de>
2021-06-01 16:33:44 +0200

    add "parse" (to modify request response (e.g. GET)) and "format" (to modifiy input value for a request (e.g. POST))

--- packages/admin/src/components/ui/inputs/NumberInput.vue
+++ packages/admin/src/components/ui/inputs/NumberInput.vue
@@ -22,6 +22,12 @@ import Editable from "../../../mixins/editable";
 export default {
   mixins: [Input, Editable],
   props: {
+    format: {
+      type: Function,
+    },
+    parse: {
+      type: Function,
+    },
     /**
      * Number to be edited.
      * @model
@@ -52,6 +58,40 @@ export default {
      * The maximum accepted value.
      */
     max: Number,
+  },
+  methods: {
+    getItem(value) {
+      let returnValue = value === undefined ? this.value : value;
+      if (typeof this.parse === "function") {
+        return this.parse(returnValue);
+      }
+      return returnValue;
+    },
+    update(value) {
+      /**
+       * Update model in the injected form if available.
+       */
+      if (this.formState) {
+        let formStateValue = value;
+        if (typeof this.format === "function") {
+          formStateValue = this.format(value);
+        }
+        this.formState.update({
+          source: this.uniqueFormId,
+          value: formStateValue,
+        });
+      }
+
+      /**
+       * Set value into input.
+       */
+      this.input = value;
+
+      /**
+       * Triggered if value updated.
+       */
+      this.$emit("input", value);
+    },
   },
 };
 </script>
