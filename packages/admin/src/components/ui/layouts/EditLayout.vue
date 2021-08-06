ce54938287a64c7aa1d2e95678fd320553efa8f4
Sebastian Klocke <info@sebastiankuhnow.de>
2021-08-06 13:31:37 +0200

    add option to disable actions in edit form

--- packages/admin/src/components/ui/layouts/EditLayout.vue
+++ packages/admin/src/components/ui/layouts/EditLayout.vue
@@ -1,12 +1,25 @@
 <template>
   <va-action-page :title="title">
     <template slot="actions">
-      <va-list-button :resource="resource" :item="item"></va-list-button>
-      <va-show-button :resource="resource" :item="item"></va-show-button>
+      <va-list-button
+        v-if="!disableList"
+        :resource="resource"
+        :item="item"
+      ></va-list-button>
+      <va-show-button
+        v-if="enableShow"
+        :resource="resource"
+        :item="item"
+      ></va-show-button>
       <!-- @slot Additional custom action buttons placeholder. -->
       <slot name="actions"></slot>
-      <va-clone-button :resource="resource" :item="item"></va-clone-button>
+      <va-clone-button
+        v-if="!disableClone"
+        :resource="resource"
+        :item="item"
+      ></va-clone-button>
       <va-delete-button
+        v-if="!disableDelete"
         :resource="resource"
         :item="item"
         redirect
@@ -27,5 +40,12 @@ import Page from "../../../mixins/page";
  */
 export default {
   mixins: [Page],
+  props: {
+    disableList: Boolean,
+    // hide "show" action by default (propably don't need it if you are in edit mode)
+    enableShow: Boolean,
+    disableClone: Boolean,
+    disableDelete: Boolean,
+  },
 };
 </script>
